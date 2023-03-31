ce63b78a84b2b2e6a23b45aecd0b128911748670
rayhan <rayhan@gmail.com>
2023-03-31 18:10:51 +0600

    Added length validation function

--- script.js
+++ script.js
@@ -14,3 +14,23 @@ eye.addEventListener("click", function () {
         eye.classList.remove("fa-eye");
     }
 })
+
+passField.addEventListener('keyup', function () {
+    validation(passField.value);
+})
+
+function validation(passValue) {
+    let length1 = document.querySelector("#length");
+    let icon = document.querySelector("#length").firstChild;
+    if (passValue.length >= 8) {
+        length1.classList.add("valid");
+        length1.classList.remove("invalid");
+        icon.classList.add("fa-check");
+        icon.classList.remove("fa-times");
+    } else {
+        length1.classList.add("invalid");
+        length1.classList.remove("valid");
+        icon.classList.add("fa-times");
+        icon.classList.remove("fa-check");
+    }
+}
